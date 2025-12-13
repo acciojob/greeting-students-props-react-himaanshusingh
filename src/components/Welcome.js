0559f70fa1f9742af5f123be12e67272c2dbe7f6
Himanshu Singh <himanshusingh1k@gmail.com>
2025-12-13 14:41:58 +0530

    commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,9 +4,8 @@ const Welcome = ({ name }) => {
   name = "Himanshu";
   return (
     <>
-      <h1>Hey !</h1>
-      <p>{name}</p>
-      <h2>Welcome to School.</h2>
+      <h1>Hey ! {name}</h1>
+      <h2>Welcome to Newton School.</h2>
     </>
   );
 };
